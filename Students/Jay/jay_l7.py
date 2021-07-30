da27ab019d959fef074ab848c1686ac9b00d9905
Timothy Simanhadi <44218116+Timmichi@users.noreply.github.com>
2021-07-30 16:18:23 -0700

    Tutoring for 7/30

--- Students/Jay/jay_l7.py
+++ Students/Jay/jay_l7.py
@@ -3,36 +3,55 @@ import time
 def intro():
     all_pokemon=["Pikachu"]*20+["Pidgeot"]*60+["Mew"]*5
     pokemon=random.choice(all_pokemon)
-    print("Aloha! Welcome to the aloha region! Get it? You are currently walking through tall grass and you encounter a "+pokemon+"! Don't waste your pokeballs! You can catch it! ")
-    choice=input("Wanna ignore and keep walking, go home, or catch it?")
+    print("You are currently walking through tall grass")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("...")
+    time.sleep(1)
+    print("... and you encounter a "+pokemon+"! Don't waste your pokeballs! You can catch it! ")
+    choice=input("Wanna ignore and keep walking, go home, or catch it (keep walking, go home, catch it)?\n")
     return choice, pokemon
-def decision(choice, pokemon, pokeballs):
+def make_choice(choice, pokemon, pokeballs):
     #This function focus on what to do when the p layer insert a choice.#
     pokedex={"Pikachu":25,"Pidgeot":50,"Mew":10}
-    if choice == 'walk' or choice == 'ignore':
+    if choice == 'keep walking' or choice == 'ignore':
         return True
-    elif choice == 'catch':
+    elif choice == 'catch it':
         percentages=pokedex[pokemon]*[True]+(100 - pokedex[pokemon])*[False]
         caught=random.choice(percentages)
+        print("You throw the pokeball and...")
+        time.sleep(1)
+        print("...")
+        time.sleep(1)
+        print("...")
         if caught:
             print("YAAAAAAAAAAAAAY!!!! YOU CAUGHT IT!!!")
         else:
-            print("WWWHHHYYYYY????? WHY, GET IN INSIDE THE POKEBALL!!!! ")
-        decision = input('Wanna continue walking or go home?')
-        if decision == "walk":
+            print("OH NO. IT GOT OUT!")
+        decision = input('Wanna keep walking or go home?')
+        if decision == "keep walking":
             return True
         else:
             return False
     else:
         return False
 
-decision = True
 
+print("Aloha! Welcome to the aloha region!")
+decision = True
 while decision:
+    
     pokeballs = 10
     choice, pokemon=intro()
-    decision=decision(choice, pokemon)
-
+    decision= make_choice(choice, pokemon, pokeballs)
+print("You walk home and go to sleep.")
+time.sleep(1)
+print("zzz")
+time.sleep(1)
+print("zzzzzzzzz")
+time.sleep(2)
+print("You are fast asleep. Goodnight!")
 
 
 
---
+++ Students/Jay/jay_l8.py
@@ -0,0 +1,12 @@
+import random
+number1 = random.randint(1,10)
+number2 = random.randint(1,10)
+print("What is " + str(number1)+ " + " + str(number2) + "?")
+answer = input()
+print(number1)
+print(number2)
+print(answer)
+if answer == (number1 + number2):
+    print("Correct!")
+else:
+    print("Nope! The answer is " + str(number1 + number2))
